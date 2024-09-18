fb70b9f76d87b9a39b937de861d98096784ff430
Pedro Vinícius <112122276+DropeVinicius@users.noreply.github.com>
2024-09-18 18:16:24 -0300

    build folder fix

--- app.js
+++ app.js
@@ -14,10 +14,10 @@ app.use('/favoritos', rotaFavorito);
 app.use('/player', rotaPlayer);
 
 // Servir arquivos estáticos do aplicativo React
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, '../client/build')));
 
  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build/index.html'));
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
 }); 
 
 const port = process.env.PORT || 8000;
